4f3fb1bc7ee2faa58691f4539ecf726941aa0021
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-25 10:04:23 -0500

    Added bootstrap
    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello</p>
+        <h1>Weather App</h1>
+        <Weather />
       </header>
+
+      <footer>
+        <span>This app was coded by </span>
+        <a href="https://www.instagram.com/creating_angelica/" target="_blank">
+          Angelica Kiolbassa
+        </a>
+        <span> and open-sourced on </span>
+        <a
+          href="https://github.com/Angelica-Kiolbassa/react-weather-app.git"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
