49982aa7b7573cb016eecaafd3650bc886104eef
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-25 17:09:41 -0500

    Cleaned up code
     removed unnecessary things and added components to the app.

--- src/weather.js
+++ src/weather.js
@@ -3,13 +3,24 @@ import "./weather.css";
 
 export default function Weather() {
   return (
-    <div classname="Weather-app">
+    <div class="Weather">
       <h1>San Antonio</h1>
-      <ul classname="List">
+      <ul>
         <li>Friday 10:12</li>
         <li>Partly Cloudy</li>
-        <li>Partly Cloudy</li>
-        <li>Partly Cloudy</li>
+        <div classname="row">
+          <div classname="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
+            98°F
+          </div>
+          <div classname="col-6">
+            <ul class="list-2">
+              <li>Precipitation: 50%</li>
+              <li>Humididty: 90%</li>
+              <li>Wind: 15 km/h%</li>
+            </ul>
+          </div>
+        </div>
       </ul>
     </div>
   );
