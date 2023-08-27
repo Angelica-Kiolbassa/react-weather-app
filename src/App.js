4ef56a799993aac5b270d67d867d56607e42bce8
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-27 13:20:25 -0500

    images and CSS
    added images and edited CSS.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "./weather.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <Weather />
       </header>
 
-      <footer classname="App-footer">
+      <footer className="footer">
         <span>This app was coded by </span>
         <a href="https://www.instagram.com/creating_angelica/" target="_blank">
           Angelica Kiolbassa
--- src/weather.js
+++ src/weather.js
@@ -4,24 +4,76 @@ import "./weather.css";
 export default function Weather() {
   return (
     <div class="Weather">
-      <h1>San Antonio</h1>
-      <ul>
-        <li>Friday 10:12</li>
-        <li>Partly Cloudy</li>
-        <div classname="row">
-          <div classname="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
-            98°F
+      <form>
+        <div className="row">
+          <div className="col-6 mb-3 ms-5 mt-1">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
           </div>
-          <div classname="col-6">
-            <ul class="list-2">
-              <li>Precipitation: 50%</li>
-              <li>Humididty: 90%</li>
-              <li>Wind: 15 km/h%</li>
-            </ul>
+          <div className="col-3 ms-0 mt-2">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-dark ps-2 ms-0"
+            />
           </div>
         </div>
-      </ul>
+      </form>
+      <h1 className="city mb-0">San Antonio</h1>
+      <div className="time">
+        <strong>Friday 10:12</strong>
+      </div>
+      <div className="temp">
+        98°
+        <span className="units">
+          F<span className="units"> |</span>
+          <span className="units"> C</span>
+        </span>
+      </div>
+      <div className="details mt-0">
+        <strong>Partly Cloudy</strong>
+      </div>
+      <div className="icon">
+        <img
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+          alt="sun"
+        />
+      </div>
+
+      <span>
+        Humididty: <span className="details">90%</span>
+        <div>
+          Wind: <span className="details">15 km/h%</span>
+        </div>
+      </span>
+
+      <div className="subtext2 mt-1">Feels like...</div>
+      <div className="headphones">
+        <img
+          src="https://www.freeiconspng.com/uploads/headphones-png-19.png"
+          id="headphones"
+          alt="Headphones pic"
+          height="200"
+          width="200"
+        ></img>
+      </div>
+      <div>
+        <iframe
+          id="playlist"
+          src="https://open.spotify.com/embed/playlist/6TzelL2zxRLsKm2d4YdhLR?utm_source=generator"
+          width="70%"
+          height="100"
+          frameborder="0"
+          allow="autoplay"
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div className="forecast mb-4">
+        <div>Daily Forcast coming soon...</div>
+      </div>
     </div>
   );
 }
