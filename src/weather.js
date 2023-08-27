f8117c4cb87e2451eff0678d173eed006a182127
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-27 17:12:44 -0500

    Added API
    Added API information, changed fake data to real time information. Updated the functions.

--- src/weather.js
+++ src/weather.js
@@ -1,82 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
-  const apiKey = d8o5aa0df3a2c34948fdac8abdta545d;
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
-  return (
-    <div class="Weather">
-      <form>
-        <div className="row">
-          <div className="col-6 mb-3 ms-5 mt-1">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-            />
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png",
+      icon: response.data.condition.icon,
+      time: response.data.time,
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div class="Weather">
+        <form>
+          <div className="row">
+            <div className="col-6 mb-3 ms-5 mt-1">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3 ms-0 mt-2">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-dark ps-2 ms-0"
+              />
+            </div>
           </div>
-          <div className="col-3 ms-0 mt-2">
-            <input
-              type="submit"
-              value="search"
-              className="btn btn-dark ps-2 ms-0"
-            />
-          </div>
+        </form>
+        <h1 className="city mb-0">{weatherData.city}</h1>
+        <div className="time">
+          <strong>{weatherData.time}</strong>
         </div>
-      </form>
-      <h1 className="city mb-0">San Antonio</h1>
-      <div className="time">
-        <strong>Friday 10:12</strong>
-      </div>
-      <div className="temp">
-        98°
-        <span className="units">
-          F<span className="units"> |</span>
-          <span className="units"> C</span>
+        <div className="temp">
+          {Math.round(weatherData.temperature)}°
+          <span className="units">
+            F<span className="units"> |</span>
+            <span className="units"> C</span>
+          </span>
+        </div>
+        <div className="details mt-0 text-capitalize">
+          <strong>{weatherData.description}</strong>
+        </div>
+        <div className="icon">
+          <img src={weatherData.icon} alt="weather icon" />
+        </div>
+
+        <span>
+          Humididty:{" "}
+          <span className="details">{Math.round(weatherData.humidity)}%</span>
+          <div>
+            Wind:{" "}
+            <span className="details">
+              {Math.round(weatherData.wind)} km/h%
+            </span>
+          </div>
         </span>
-      </div>
-      <div className="details mt-0">
-        <strong>Partly Cloudy</strong>
-      </div>
-      <div className="icon">
-        <img
-          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-          alt="sun"
-        />
-      </div>
 
-      <span>
-        Humididty: <span className="details">90%</span>
-        <div>
-          Wind: <span className="details">15 km/h%</span>
+        <div className="subtext2 mt-1">Feels like...</div>
+        <div className="headphones">
+          <img
+            src="https://www.freeiconspng.com/uploads/headphones-png-19.png"
+            id="headphones"
+            alt="Headphones pic"
+            height="200"
+            width="200"
+          ></img>
         </div>
-      </span>
+        <div>
+          <iframe
+            id="playlist"
+            src="https://open.spotify.com/embed/playlist/6TzelL2zxRLsKm2d4YdhLR?utm_source=generator"
+            width="70%"
+            height="100"
+            frameborder="0"
+            allow="autoplay"
+            loading="lazy"
+          ></iframe>
+        </div>
+        <div className="forecast mb-4">
+          <div>Daily Forcast coming soon...</div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "d8o5aa0df3a2c34948fdac8abdta545d";
+    let city = "San Antonio";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-      <div className="subtext2 mt-1">Feels like...</div>
-      <div className="headphones">
-        <img
-          src="https://www.freeiconspng.com/uploads/headphones-png-19.png"
-          id="headphones"
-          alt="Headphones pic"
-          height="200"
-          width="200"
-        ></img>
-      </div>
-      <div>
-        <iframe
-          id="playlist"
-          src="https://open.spotify.com/embed/playlist/6TzelL2zxRLsKm2d4YdhLR?utm_source=generator"
-          width="70%"
-          height="100"
-          frameborder="0"
-          allow="autoplay"
-          loading="lazy"
-        ></iframe>
-      </div>
-      <div className="forecast mb-4">
-        <div>Daily Forcast coming soon...</div>
-      </div>
-    </div>
-  );
+    return "Loading... ";
+  }
 }
