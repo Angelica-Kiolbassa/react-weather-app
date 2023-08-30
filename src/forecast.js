d6114760668f41521e0096b7bacd6a1e4a1cdcc1
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-30 16:52:55 -0500

    Started building the forecast daily
    Added data in progress of building API data for daily forecast.

--- src/forecast.js
+++ src/forecast.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./forecast.css";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily[0]);
+    setLoaded(true);
   }
 
-  const apiKey = "d8o5aa0df3a2c34948fdac8abdta545d";
-  let lon = props.coordinates.longitude;
-  let lat = props.coordinates.latitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="forecast">
+        <div className="row">
+          <div className="col">
+            <div className="forecast-day">{forecast.time}</div>
 
-  axios.get(apiUrl).then(handleResponse);
+            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/{forecast.condition.icon}.png" />
 
-  return (
-    <div className="forecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Thu</div>
-          <img
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-            alt="weather icon"
-            height="60"
-            width="60"
-            className="forecast-icon"
-          />
-          <div className="forecast-temp-max">19°</div>
-          <div className="forecast-temp-min">10°</div>
+            <div className="forecast-temp-max">
+              {forecast.temperature.maximum}°
+            </div>
+            <div className="forecast-temp-min">
+              {forecast.temperature.minimum}°
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "d8o5aa0df3a2c34948fdac8abdta545d";
+    let lon = props.coordinates.longitude;
+    let lat = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
