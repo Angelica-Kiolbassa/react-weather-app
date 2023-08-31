273c104372b7460ea6be62b28946a7ab71aa820c
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-08-31 11:32:14 -0500

    Added day to forecast
    Added day for forecast.

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,14 +1,33 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemp = Math.round(props.data.temperature.maximum);
+    return `${maxTemp}°`;
+  }
+
+  function minTemperature() {
+    let minTemp = Math.round(props.data.temperature.minimum);
+    return `${minTemp}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.temperature.day * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="forecast-day">{props.data.temperature.day}</div>
+      <div className="forecast-day">{day()}</div>
 
       <img src={props.data.condition.icon_url} height="50" width="50" />
 
-      <div className="forecast-temp-max">{props.data.temperature.maximum}°</div>
-      <div className="forecast-temp-min">{props.data.temperature.minimum}°</div>
+      <div className="forecast-temp-max">{maxTemperature()}</div>
+      <div className="forecast-temp-min">{minTemperature()}</div>
     </div>
   );
 }
